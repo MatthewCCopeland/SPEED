b012a7184b7a05b767a35fe018caa6ba2a0f9aa1
MatthewCCopeland <mattyccopeland@gmail.com.com>
2022-05-18 23:45:59 +1200

    commit message

--- speed/server.js
+++ speed/server.js
@@ -3,11 +3,22 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/article"));
 // get driver connection
 const dbo = require("./db/conn");
+
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.listen(port, () => {
     // perform a database connection when server starts
