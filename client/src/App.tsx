2ebb0fa79147d6c4ee35c8c77e2b42dd3a0dc4bd
MatthewCCopeland <mattyccopeland@gmail.com.com>
2022-06-03 08:53:27 +1200

    Removed localhost

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,7 @@ import { AnalysisList, AnalysisShow, AnalysisEdit } from "pages/analysis";
 const App: React.FC = () => {
   return (
     <Refine
-      dataProvider={dataProvider("")}
+      dataProvider={dataProvider("https://speed-develop.herokuapp.com")}
       routerProvider={routerProvider}
       resources={[
         {
